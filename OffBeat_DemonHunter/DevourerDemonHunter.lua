a59b384d47381a20ff56091b6a635218f07a5e0b
alde <r.dybeck@gmail.com>
2026-08-02 14:53:30 -0400

    feat: flesh out Devourer profile from Method guide
    Added Void Meta transformations (Devour, Cull), Eradicate,
    Vengeful Retreat, and Immolation Aura to rotation tracking.
    Added Voidfall (Annihilator stacks) and Moment of Craving
    (Void Ray resets Reap) to tracked auras. Added Voidblade and
    The Hunt to idle cooldown alerts. Added proc tracking for
    Moment of Craving consumption via Reap.

--- OffBeat_DemonHunter/DevourerDemonHunter.lua
+++ OffBeat_DemonHunter/DevourerDemonHunter.lua
@@ -11,8 +11,11 @@ OffBeat:RegisterProfile({
 
     rotationSpells = {
         { spellId = 473662 },  -- Consume
+        { spellId = 1217610 }, -- Devour (Consume during Void Meta)
         { spellId = 473728 },  -- Void Ray
         { spellId = 1226019 }, -- Reap
+        { spellId = 1245453 }, -- Cull (Reap during Void Meta)
+        { spellId = 1225826 }, -- Eradicate (replaces Reap when talented)
         { spellId = 1217605 }, -- Void Metamorphosis
         { spellId = 1221150 }, -- Collapsing Star
         { spellId = 1241937 }, -- Soul Immolation
@@ -20,12 +23,16 @@ OffBeat:RegisterProfile({
         { spellId = 1239519 }, -- Hungering Slash
         { spellId = 1246167 }, -- The Hunt (Devourer)
         { spellId = 1234796 }, -- Shift
+        { spellId = 198793 },  -- Vengeful Retreat
+        { spellId = 258920 },  -- Immolation Aura
         { spellId = 185123 },  -- Throw Glaive
         { spellId = 204596 },  -- Sigil of Flame
     },
 
     trackedAuras = {
-        { spellId = 1217605, name = "Void Metamorphosis", baseDuration = 30, stacks = false },
+        { spellId = 1217605, name = "Void Metamorphosis",  baseDuration = 30, stacks = false },
+        { spellId = 1253304, name = "Voidfall",            baseDuration = 30, stacks = true },
+        { spellId = 1238488, name = "Moment of Craving",   baseDuration = 8,  stacks = false },
     },
 
     keyCooldown = {
@@ -36,6 +43,17 @@ OffBeat:RegisterProfile({
 
     idleCooldowns = {
         { spellId = 1241937, name = "Soul Immolation" },
+        { spellId = 1245412, name = "Voidblade" },
+        { spellId = 1246167, name = "The Hunt" },
+    },
+
+    procTracking = {
+        {
+            procAura = 1238488,
+            consumeSpell = 1226019,
+            window = 0.5,
+            name = "Moment of Craving",
+        },
     },
 
     castAnnouncements = {
